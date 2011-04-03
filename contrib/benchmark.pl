e866d01632ca585c5f99faf3dee6f17080341663
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2011-04-03 17:50:29 +0000

    benchmark modified for new code

--- contrib/benchmark.pl
+++ contrib/benchmark.pl
@@ -3,23 +3,21 @@ use strict;
 use warnings;
 use Linux::Smaps;
 use Linux::Smaps::Tiny;
+use Linux::Smaps::Tiny::PP;
 use Benchmark qw/:all :hireswallclock/;
 
-cmpthese(1000, {
+cmpthese(10000, {
     "Linux::Smaps" => sub {
         Linux::Smaps->new->all;
         return;
     },
     "Linux::Smaps::Tiny" => sub {
-        Linux::Smaps::Tiny::get_smaps_summary;
+        Linux::Smaps::Tiny::get_smaps_summary();
         return;
     },
-    "Linux::Smaps::Tiny::XS (stdio)" => sub {
-        Linux::Smaps::Tiny::get_smaps_summary_xs;
+    "Linux::Smaps::Tiny::PP" => sub {
+        Linux::Smaps::Tiny::PP::__get_smaps_summary();
         return;
     },
-    "Linux::Smaps::Tiny::XS (slurp)" => sub {
-        Linux::Smaps::Tiny::get_smaps_summary_slurp;
-        return;
-    },
+
 });
